a44c79ea1315a9ad1c4d5013137abf495d78f80c
ImmenseHorse <manhducams@gmail.com>
2021-07-16 21:31:45 +0700

    submit Test

--- Problem B/B.js
+++ Problem B/B.js
@@ -4,7 +4,7 @@ function pickRandomColor() {
     let randomHex = randomColor.toString(16);
     let colorDisplay = '#' + randomHex;
     document.body.style.backgroundColor = colorDisplay;
-    document.getElementById('tBody').insertAdjacentHTML('beforeend', `<div>The color on display is <input type= "text" id="tColor" value=${colorDisplay}><button id="copyCol">Copy</button></div>`);
+    document.getElementById('tBody').insertAdjacentHTML('beforeend', `<div><input id="styleShow" readonly value="The color on display is" ><input type= "text" id="tColor" value=${colorDisplay}><button id="copyCol">Copy</button></div>`);
     document.getElementById('copyCol').addEventListener('click', () => {
         document.getElementById('tColor').select();
         document.execCommand("copy");
